4ba994b49c47bd7addb9f990e14f34fd0a6916ee
Tom Berger <berger.tom@mail.de>
2025-11-21 10:08:47 +0100

    feat: pass along state to room id

--- src/controller/game.controller.ts
+++ src/controller/game.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
 import {SocketService} from "../services/socket.service";
+import {StateUpdateModel} from "../models/state-update.model";
 
 export class GameController {
     static async updateState(req: Request, res: Response) {
-        console.log(req.body);
+        const data = req.body as StateUpdateModel;
 
-        SocketService.getInstance().emitToRoom('abc', 'update-state', req.body);
+        SocketService.getInstance().emitToRoom(data.sessionId, 'update-state', data.state);
 
         return res.status(200).send();
     }
